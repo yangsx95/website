990a3a3b0f4e3323503cac6ce8beaa4dc49896f4
杨顺翔 <yangsx95@qq.com>
2025-02-06 14:07:01 +0800

    update

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -78,7 +78,7 @@ const config: Config = {
           type: 'docSidebar',
           sidebarId: 'tutorialSidebar',
           position: 'left',
-          label: 'Tutorial',
+          label: '生活',
         },
         {to: '/blog', label: 'Blog', position: 'left'},
         {
